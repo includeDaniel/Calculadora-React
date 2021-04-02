834f7dff7f32a62b6421b71c71bcd7c181420ceb
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-02 14:56:41 -0300

    Component Calculator

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1>Calculadora</h1>
+      <Calculator />
+  </div>, 
   document.getElementById('root')
 );
 
---
+++ src/main/Calculator.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Calculator.css'
+
+export default class Calculator extends Component {
+    render() {
+        return(
+            <div className="calculator">
+
+            </div>
+        )
+    }
+}
