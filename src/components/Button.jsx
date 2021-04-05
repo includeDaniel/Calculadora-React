562426b590dd6249225763f1f5a2f96605369ec6
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-05 16:25:59 -0300

    calculator layout

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,14 @@ import './Button.css'
 
 export default function Botao(props) {
     return(
-        <button className="button">{props.label}</button>
+        <button onClick={e => props.click(props.label)} className={`
+            button
+            ${props.operation ? 'operation' : ''}
+            ${props.double ? 'double' : ''}
+            ${props.triple ? 'triple' : ''}  
+        `}
+        >
+            {props.label}
+        </button>
     )
 }
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,37 +4,47 @@ import Button from '../components/Button'
 import Display from '../components/Display'
 
 export default class Calculator extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+    }
+
+    clearMemory() {
+        console.log('limpar')
+    }
+    setOperation(operation) {
+        console.log(operation)
+    }
+    addDigit(n) {
+        console.log(n)
+    }
+
     render() {
+
         return(
             <div className="calculator">
                 <Display value={100}/>
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
-                <Button label="AC" />
+                <Button label="AC" click={this.clearMemory} triple/>
+                <Button label="/" click={this.setOperation} operation/>
+                <Button label="7" click={this.addDigit} />
+                <Button label="8" click={this.addDigit} />
+                <Button label="9" click={this.addDigit} />
+                <Button label="*" click={this.addDigit} operation/>
+                <Button label="4" click={this.addDigit} />
+                <Button label="5" click={this.addDigit} />
+                <Button label="6" click={this.addDigit} />
+                <Button label="-" click={this.addDigit} operation/>
+                <Button label="1" click={this.addDigit} />
+                <Button label="2" click={this.addDigit} />
+                <Button label="3" click={this.addDigit} />
+                <Button label="+" click={this.addDigit} operation/>
+                <Button label="0" click={this.addDigit} double/>
+                <Button label="." click={this.addDigit} />
+                <Button label="=" click={this.addDigit} operation/>
             </div>
         )
     }
